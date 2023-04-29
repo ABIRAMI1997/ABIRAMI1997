51448f0105f709cbdb1a0f262026f22dd2dc3d11
Abirami Ramachandran <63499457+ABIRAMI1997@users.noreply.github.com>
2023-04-29 14:39:48 +0530

    Add files via upload

---
+++ shell_scripts/Cnt_files_with_dynamic_pattern.sh
@@ -0,0 +1,33 @@
+# Command line : sh sh Cnt_files_with_ABC_pattern.sh abi
+#abi = pattern needes to be searched
+
+#!/bin/bash
+
+
+#logs
+curr="$(pwd)/archieve.log"
+exec 2>"$curr"
+set -x
+
+#dynamic pattern
+pattern="$1"
+
+#append file name with today's date
+date=$(date +"%Y-%m-%d")
+
+# Set the search path and pattern
+path=$(pwd)
+#pattern="ABC"
+
+#count of files with pattern
+count=$(ls -1 "$pattern"* | wc -l)
+echo "The number of files starting with $pattern in the current directory is: $count"
+
+#rename the file with today's date
+for file in "$pattern"*; do
+	echo "$file"
+    mv "$file" "${file%.*}_$date.${file##*.}"
+done
+
+set +x
+
